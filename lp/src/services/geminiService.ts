f37b9fdda599ff012f34da2f04947cd3de449356
yaroslav <striliuk.yaroslav@gmail.com>
2026-01-29 18:12:20 +0100

    Delete lp directory

--- lp/src/services/geminiService.ts
+++
@@ -1,6 +0,0 @@
-import { InventoryState } from "../types";
-
-// Service is currently disabled/unused
-export const analyzeInventory = async (inventory: InventoryState): Promise<string> => {
-  return "";
-};
